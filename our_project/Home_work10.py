a48fcda95b272cab5352a12601f81a86591528a5
Dmytro Lyfenko <d.lyfenko@gmail.com>
2022-12-25 17:14:39 +0200

    Home_work10 Work

---
+++ our_project/Home_work10.py
@@ -0,0 +1,58 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self,value):
+        self.value = value
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    pass
+
+class Record:
+    def __init__(self, name: Name, phone: Phone = None):
+        self.name = name
+        self.phones = []
+        if isinstance(phone, Phone):
+            self.phones.append(phone)
+
+    def show_contact(self):
+        return {"name": self.name, "phone": self.phones}
+
+    def add_phone(self, phone):
+        self.phone.append(phone)
+
+    def change_phone(self, phone):
+        self.phones = phone
+
+    def del_phone(self):
+        self.phones = []
+
+
+class AddressBook(UserDict):
+
+    def add_record(self, record: Record):
+        self.data[record.name.value] = record
+
+    def remove_record(self, record):
+        self.data.pop(record.name.value, None)
+
+    def show_records(self):
+        return self.data
+
+
+if __name__ == '__main__':
+    name = Name('Bill')
+    phone = Phone('1234567890')
+    rec = Record(name, phone)
+    ab = AddressBook()
+    ab.add_record(rec)
+
+    assert isinstance(ab['Bill'], Record)
+    assert isinstance(ab['Bill'].name, Name)
+    assert isinstance(ab['Bill'].phones, list)
+    assert isinstance(ab['Bill'].phones[0], Phone)
+    assert ab['Bill'].phones[0].value == '1234567890'
+
+    print('All Ok)')
